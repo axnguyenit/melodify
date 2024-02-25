fd5f53fc55761adf9c5797b3d42750e6657f5ae1
Kha <axnguyen.it@gmail.com>
2024-02-25 19:26:51 +0700

    update: android

--- app/android/app/src/main/kotlin/com/n4/app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.n4.melodify
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {
-}
---
+++ app/android/app/src/main/kotlin/com/n4/melodify/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.n4.melodify
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity : FlutterActivity() {
+//	override fun onCreate(savedInstanceState: Bundle?) {
+//		super.onCreate(savedInstanceState);
+//		GeneratedPluginRegistrant.registerWith(this);
+//
+//		// Tạo notification channel
+//		NotificationChannel channel = new NotificationChannel("app_channel", "App notifications", NotificationManager.IMPORTANCE_DEFAULT);
+//		channel.setDescription("This channel is used for app notifications");
+//
+//		NotificationManager notificationManager = getSystemService(NotificationManager.class);
+//		notificationManager.createNotificationChannel(channel);
+//	}
+}
