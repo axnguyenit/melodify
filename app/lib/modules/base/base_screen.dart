d4e2a547ceeaee78f04be358bf9f96c0b46aa6a0
Kha <axnguyen.it@gmail.com>
2024-03-08 19:42:05 +0700

    update: base screen

--- app/lib/modules/base/base_screen.dart
+++ app/lib/modules/base/base_screen.dart
@@ -6,25 +6,41 @@ import 'package:melodify/modules/mixins/mixins.dart';
 
 abstract class BaseScreen<T extends StatefulWidget> extends State<T>
     with StateMixin, SessionData, ToastShowing {
+  @protected
   final router = Routing();
 
+  @protected
   bool get extendBodyBehindAppBar => false;
 
+  @protected
   bool get resizeToAvoidBottomInset => true;
 
+  @protected
   PreferredSizeWidget? get buildAppBar => null;
 
+  @protected
   Widget get buildBody;
 
+  @protected
   Widget? get buildBottomNavigationBar => null;
 
+  @protected
   Widget? get buildFloatingActionButton => null;
 
+  @protected
   FloatingActionButtonLocation get floatingActionButtonLocation =>
       FloatingActionButtonLocation.centerFloat;
 
-  double get statusBarHeight => AppConstants.statusBarHeight;
+  @protected
+  double get statusBarHeight => MediaQuery.paddingOf(context).top;
 
+  @protected
+  double get screenWidth => MediaQuery.sizeOf(context).width;
+
+  @protected
+  double get screenHeight => MediaQuery.sizeOf(context).height;
+
+  @protected
   double get horizontalPadding => AppConstants.appPadding;
 
   @override
